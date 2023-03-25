76db6e16173179e87c630f73ed59fb11f889e862
daniel <arlertdaniel@gmail.com>
2023-03-25 10:52:10 +0100

    feat(accounts): add custom user model

---
+++ accounts/tests.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class UsersManagersTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            email="john@doe.com",
+            password="123456",
+        )
+
+        self.assertEqual(user.email, "john@doe.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
