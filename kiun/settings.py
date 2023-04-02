c7e3a04df3315e437954e9fbda0a51e8f9c90b69
daniel <arlertdaniel@gmail.com>
2023-04-02 18:11:46 +0200

    feat: change default permission for drf

--- kiun/settings.py
+++ kiun/settings.py
@@ -75,7 +75,7 @@ CSRF_TRUSTED_ORIGINS = []
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
 }
 
--- kiun/urls.py
+++ kiun/urls.py
@@ -3,8 +3,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/accounts/", include("accounts.urls")),
-    path("api/projects/", include("projects.urls")),
-    path("api/skills/", include("skills.urls")),
-    path("api/studies/", include("studies.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/v1/accounts/", include("accounts.urls")),
+    path("api/v1/projects/", include("projects.urls")),
+    path("api/v1/skills/", include("skills.urls")),
+    path("api/v1/studies/", include("studies.urls")),
 ]
